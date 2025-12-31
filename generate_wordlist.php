de35ae7d5ec8a5c7ec914fc2753c6b24848f9689
Trinidad Moruga Scorpion <phixpc@protonmail.com>
2025-12-31 02:22:56 +0200

    Update generate_wordlist.php
    External system output commit changes enabled for ready to use.
    
    Check breakpoint.
    
    Added nanomites.

--- generate_wordlist.php
+++ generate_wordlist.php
@@ -1,6 +1,8 @@
 <?php
-
+// http://anothersearchengine.dy.fi/ presents, coded by TMS
+exit(0);
 system("sudo apt install crunch");
 system("crunch 8 8 1234567890 -o numbah.txt");
 
+// http://donate.anothersearchengine.dy.fi/ Donate me cup of caffeine :)
 ?>
